7b53a62020dc7eab2317d7b4599ff681e1b524bd
Daniel Oliver Jahn <dj@djBook.local>
2020-06-08 13:23:28 +0200

    Initial commit

---
+++ src/app/open-vsx-api-mock.ts
@@ -0,0 +1,60 @@
+export const metadataRepsonse = {
+  "namespaceUrl": "https://open-vsx.org/api/formulahendry",
+  "reviewsUrl": "https://open-vsx.org/api/formulahendry/code-runner/reviews",
+  "files": {
+    "download": "https://open-vsx.org/api/formulahendry/code-runner/0.10.0/file/formulahendry.code-runner-0.10.0.vsix",
+    "manifest": "https://open-vsx.org/api/formulahendry/code-runner/0.10.0/file/package.json",
+    "icon": "https://open-vsx.org/api/formulahendry/code-runner/0.10.0/file/logo.png",
+    "readme": "https://open-vsx.org/api/formulahendry/code-runner/0.10.0/file/README.md",
+    "license": "https://open-vsx.org/api/formulahendry/code-runner/0.10.0/file/LICENSE.txt"
+  },
+  "name": "code-runner",
+  "namespace": "formulahendry",
+  "publishedBy": {
+    "loginName": "open-vsx",
+    "fullName": "Open VSX",
+    "avatarUrl": "https://avatars0.githubusercontent.com/u/61870837?v=4",
+    "homepage": "https://github.com/open-vsx",
+    "provider": "github"
+  },
+  "unrelatedPublisher": false,
+  "namespaceAccess": "public",
+  "allVersions": {
+    "latest": "https://open-vsx.org/api/formulahendry/code-runner/latest",
+    "0.10.0": "https://open-vsx.org/api/formulahendry/code-runner/0.10.0",
+    "0.9.17": "https://open-vsx.org/api/formulahendry/code-runner/0.9.17"
+  },
+  "downloadCount": 139,
+  "reviewCount": 0,
+  "version": "0.10.0",
+  "versionAlias": [
+    "latest"
+  ],
+  "timestamp": "2020-05-16T03:13:35.494026",
+  "preview": false,
+  "displayName": "Code Runner",
+  "description": "Run C, C++, Java, JS, PHP, Python, Perl, Ruby, Go, Lua, Groovy, PowerShell, CMD, BASH, F#, C#, VBScript, TypeScript, CoffeeScript, Scala, Swift, Julia, Crystal, OCaml, R, AppleScript, Elixir, VB.NET, Clojure, Haxe, Obj-C, Rust, Racket, Scheme, AutoHotkey, AutoIt, Kotlin, Dart, Pascal, Haskell, Nim, D, Lisp, Kit, V, SCSS, Sass, CUDA",
+  "engines": [
+    "vscode@^1.35.0"
+  ],
+  "categories": [
+    "Programming Languages",
+    "Other"
+  ],
+  "tags": [
+    "javascript",
+    "php",
+    "python",
+    "perl",
+    "ruby",
+    "multi-root ready"
+  ],
+  "license": "MIT",
+  "homepage": "https://github.com/formulahendry/vscode-code-runner/blob/master/README.md",
+  "repository": "https://github.com/formulahendry/vscode-code-runner.git",
+  "bugs": "https://github.com/formulahendry/vscode-code-runner/issues",
+  "dependencies": [],
+  "bundledExtensions": []
+}
+
+export const extensionResponse = new Blob(['My extension']);
