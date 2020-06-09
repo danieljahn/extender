22d418cb72c57060e68a939052f953bea9dbdd6d
Daniel Jahn <370305+danieljahn@users.noreply.github.com>
2020-06-09 02:11:11 +0200

    #1: Skip non existing extension IDs (#3)
    #1: Skip non existing extension IDs

--- src/app/extension.spec.ts
+++ src/app/extension.spec.ts
@@ -1,4 +1,4 @@
-import { Extension } from './extension';
+import {Extension, ExtensionMetadataError} from './extension';
 // Http testing module and mocking controller
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
@@ -7,28 +7,49 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 describe('Extension', () => {
-  it('should create an instance from an extension id', () => {
-    const extension: Extension = Extension.from('namespace.extension');
 
-    expect(extension).toBeTruthy();
-    expect(extension.id).toEqual('namespace.extension');
+  describe('from()', () => {
+    it('should create an instance from an extension id', () => {
+      const extension: Extension = Extension.from('namespace.extension');
+
+      expect(extension).toBeTruthy();
+      expect(extension.id).toEqual('namespace.extension');
+    });
+
+    it('should not create an instance from an empty extension id',  () => {
+      const extension: Extension = Extension.from('');
+
+      expect(extension).toBeFalsy();
+    });
+
+    it('should should extract namespace from id', () => {
+      const extension: Extension = Extension.from('namespace.extension');
+
+      expect(extension.namespace).toEqual('namespace');
+    });
+
+    it('should should extract name from id', () => {
+      const extension: Extension = Extension.from('namespace.extension');
+
+      expect(extension.name).toEqual('extension');
+    });
   });
 
-  it('should not create an instance from an empty extension id',  () => {
-    const extension: Extension = Extension.from('');
+  describe('hasError()', () => {
+    it('should report errors if errors exist', () => {
+      const extension = Extension
+        .from('extension.withrrors')
+        .withError(new ExtensionMetadataError());
 
-    expect(extension).toBeFalsy();
-  });
+      expect(extension.hasErrors()).toEqual(true);
+    });
 
-  it('should should extract namespace from id', () => {
-    const extension: Extension = Extension.from('namespace.extension');
+    it('should report no errors if no errors exist', () => {
+      const extension = Extension.from('extension.withouterrors');
 
-    expect(extension.namespace).toEqual('namespace');
-  });
-
-  it('should should extract name from id', () => {
-    const extension: Extension = Extension.from('namespace.extension');
-
-    expect(extension.name).toEqual('extension');
+      expect(extension.hasErrors()).toEqual(false);
+    });
   });
 });
+
+
